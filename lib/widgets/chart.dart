56941b99c2c41f5e32976b4979c17c60a8269c01
shubham-sapkal <sapkal.shubham2002@gmail.com>
2021-09-22 00:12:14 +0530

    added chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expenses_app/models/transactions.dart';
 import 'package:intl/intl.dart';
+import 'package:personal_expenses_app/widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -13,20 +14,29 @@ class Chart extends StatelessWidget {
         Duration(days: index),
       );
 
-      double totalSum=0.0;
+      double totalSum = 0.0;
 
       for (var i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
             recentTransactions[i].date.year == weekDay.year) {
-              totalSum += recentTransactions[i].amount;
-            }
+          totalSum += recentTransactions[i].amount;
+        }
       }
 
       // print('Weekday: '+ DateFormat.E().format(weekDay).toString());
       // print('totalSum: $totalSum');
 
-      return {'day': DateFormat.E().format(weekDay).toString(), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).toString(),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + double.parse(item['amount'].toString());
     });
   }
 
@@ -34,11 +44,27 @@ class Chart extends StatelessWidget {
   Widget build(BuildContext context) {
     // print(groupedTransactionValues);
     return Card(
-        // child: Text('CHART!'),
-        elevation: 6,
-        margin: EdgeInsets.all(20),
+      // child: Text('CHART!'),
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: EdgeInsets.all(10),
         child: Row(
-          children: <Widget>[],
-        ));
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((data) {
+            // print('Spending amount 1 : ${double.parse(data['amount'].toString())/totalSpending} ');
+            // print('Spending amount condition 1 : ${double.parse(data['amount'].toString())/totalSpending == null ? 0 : 1} ');
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: data['day'].toString(),
+                spendingAmount: double.parse(data['amount'].toString()),
+                spendingPercentTotal: totalSpending == 0.0? 0.0 : double.parse(data['amount'].toString())/totalSpending 
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentTotal;
+
+  ChartBar(
+      {required this.label,
+      required this.spendingAmount,
+      this.spendingPercentTotal = 0});
+
+  double get spendingper {
+    return spendingPercentTotal;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FittedBox(
+          child: Text('₹${spendingAmount.toStringAsFixed(0)}'),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPercentTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
